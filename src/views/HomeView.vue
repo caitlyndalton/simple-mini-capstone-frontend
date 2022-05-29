18661086c0c9505d89f796074787b128257939aa
Caitlyn Dalton <cdalton0610@gmail.com>
2022-05-29 12:49:36 -0400

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
+<script>
+var axios = require("axios");
+
+export default {
+  data: function () {
+    return {
+      products: [],
+    };
+  },
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("http://localhost:3000/products.json").then((response) => {
+        console.log("All products", response.data);
+        this.products = response.data;
+      });
+    },
+  },
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>All Products</h1>
+    <div v-for="product in products" v-bind:key="product.id">
+      <h2>Name: {{ product.name }}</h2>
+      <p>Price: {{ product.price }}</p>
+      <p>Image: {{ product.image_url }}</p>
+      <img v-bind:src="product.image_url" v-bind:alt="product.name" />
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+<style>
+.home img {
+  width: 100px;
 }
-</script>
+</style>
